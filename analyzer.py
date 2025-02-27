f2671abbb3d177081b1bcf689612c5bbb82cee90
Устенко Иван Павлович <ipustenko@edu.hse.ru>
2025-02-27 16:36:07 +0300

    addad days parametr, added topic_loc, bin_conduit

--- analyzer.py
+++ analyzer.py
@@ -1,15 +1,28 @@
 import numpy as np
 import pandas as pd
 
-def competative_points(df: pd.DataFrame, topic="all", subtopic="all", weight_func="sqrt") -> pd.Series:
-    df = df.drop(columns=['Процент порядка', 'Процент номера'])
+def topic_loc(df: pd.DataFrame, topic="all", subtopic="all") -> pd.DataFrame:
     if topic != "all":
         df = df.loc[df["Тема"] == topic]
     if subtopic != "all":
         df = df.loc[df["Подтема"] == subtopic]
     df = df.drop(columns=['Тема', 'Подтема'])
+    return df
+
+def bin_conduit(df: pd.DataFrame, days = "all") -> pd.DataFrame:
+    df = df.drop(columns=['Процент порядка', 'Процент номера'])
     df = df.astype(float)
-    df = df.replace([2,3,4,5,6,7,8], 1)
+    if days == "all":
+        df = df.replace([2,3,4,5,6,7,8], 1)
+    else:
+        df = df.replace(days, -1)
+        df = df.replace([1,2,3,4,5,6,7,8], 0)
+        df = df.replace(-1,1) #потом переделать
+    return df
+
+def competative_points(df: pd.DataFrame, topic="all", subtopic="all", weight_func="sqrt", days="all") -> pd.Series:
+    df = topic_loc(df, topic, subtopic)
+    df = bin_conduit(df, days)
     df = df[df.sum(axis=1) != 0]
     if weight_func == "sqrt":
         df['Weights'] = 1/np.sqrt(df.sum(axis=1))
@@ -20,18 +33,11 @@ def competative_points(df: pd.DataFrame, topic="all", subtopic="all", weight_fun
         df.iloc[:, :-1] = df.iloc[:, :-1].mul(df['Weights'], axis=0)
         return df.iloc[:, :-1].sum()
 
-def total_problems(df: pd.DataFrame) -> pd.Series:
-    return df.sum()
+def total_problems(df: pd.DataFrame, topic="all", subtopic="all", days="all") -> pd.Series:
+    return bin_conduit(topic_loc(df, topic, subtopic), days).sum()
 
-def work_points(df: pd.DataFrame, topic="all", subtopic="all", weight_func="linear") -> pd.Series:
-    df = df.drop(columns=['Процент порядка', 'Процент номера'])
-    if topic != "all":
-        df = df.loc[df["Тема"] == topic]
-    if subtopic != "all":
-        df = df.loc[df["Подтема"] == subtopic]
-    df = df.drop(columns=['Тема', 'Подтема'])
-    df = df.astype(float)
-    df = df.replace([2,3,4,5,6,7,8], 1)
+def work_points(df: pd.DataFrame, topic="all", subtopic="all", weight_func="linear", days="all") -> pd.Series:
+    df = bin_conduit(topic_loc(df, topic, subtopic), days)
     df = df[df.sum(axis=1) != 0]
     if weight_func == "sqrt":
         df['Weights'] = np.sqrt(df.sum(axis=1))
